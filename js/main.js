df48dc51c2ec77df2cafdc87944e31556d49c936
fazioandrea.f <fazioandrea.f@gmail.com>
2021-03-11 16:35:29 +0100

    coupon valido senza il bonus

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var calculatejs = document.getElementById('calculate');
-var coupon: [ CaretPosition, forzacatania, ]
-
+var couponArr= [ 'BRS4AQ9CW6', 'LALUJ3ER9P', '2FTGNQVGRC', '374HKEZ243']
+var priceTot;
 calculatejs.addEventListener('click',function(){
     var priceTot=5;
     var namejs = document.getElementById('name');
@@ -8,13 +8,27 @@ calculatejs.addEventListener('click',function(){
         alert('You not write the name of burger, please retry')
     }
     var burgerName = namejs.value;
-    var liIngredients = document.getElementsByClassName('checkboxIngredients')
-    console.dir(burgerName,liIngredients)
+    var liIngredients = document.getElementsByClassName('checkboxIngredients');
     for (i=0; i<liIngredients.length;i++){
         var liIngredientsOnce=liIngredients[i];
         if (liIngredientsOnce.checked){
             priceTot += parseInt(liIngredientsOnce.dataset.price)
         }
-        console.log( priceTot)
     }
+    // parte del coupon
+    var couponjs= document.getElementById('coupon').value;
+    console.log(couponjs);
+    for(i=0; i<couponArr.length;i++) {
+        if(couponjs==couponArr[i]) {
+            priceTot = Math.round(priceTot*0.8);
+
+        }
+    }
+    // occuparci nell eliminare l'elemneto coupon gia usato
+
+
+
+
+    console.log(priceTot);
+    document.getElementById('finalprice').innerHTML = '€' + priceTot;
 })
